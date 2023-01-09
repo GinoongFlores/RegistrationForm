3b8bbfcf41a7463b53b68161d503120bae69ab25
GinoongFlores <Donie@DESKTOP-N6KL3IJ>
2023-01-09 20:57:14 +0800

    added Register form

---
+++ RegistrationForm/Register.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace RegistrationForm
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBxUsername = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBxPassword = new System.Windows.Forms.TextBox();
+            this.txtBxConfirmPassword = new System.Windows.Forms.TextBox();
+            this.chkBxShowPassword = new System.Windows.Forms.CheckBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.backToLogin = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.label1.Location = new System.Drawing.Point(25, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.label2.Location = new System.Drawing.Point(25, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.label3.Location = new System.Drawing.Point(25, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Confirm Password";
+            // 
+            // txtBxUsername
+            // 
+            this.txtBxUsername.Location = new System.Drawing.Point(29, 129);
+            this.txtBxUsername.Multiline = true;
+            this.txtBxUsername.Name = "txtBxUsername";
+            this.txtBxUsername.Size = new System.Drawing.Size(240, 27);
+            this.txtBxUsername.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Console", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(25, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 21);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Get Started";
+            // 
+            // txtBxPassword
+            // 
+            this.txtBxPassword.Location = new System.Drawing.Point(29, 202);
+            this.txtBxPassword.Multiline = true;
+            this.txtBxPassword.Name = "txtBxPassword";
+            this.txtBxPassword.PasswordChar = '*';
+            this.txtBxPassword.Size = new System.Drawing.Size(240, 27);
+            this.txtBxPassword.TabIndex = 2;
+            // 
+            // txtBxConfirmPassword
+            // 
+            this.txtBxConfirmPassword.Location = new System.Drawing.Point(29, 277);
+            this.txtBxConfirmPassword.Multiline = true;
+            this.txtBxConfirmPassword.Name = "txtBxConfirmPassword";
+            this.txtBxConfirmPassword.PasswordChar = '*';
+            this.txtBxConfirmPassword.Size = new System.Drawing.Size(240, 27);
+            this.txtBxConfirmPassword.TabIndex = 3;
+            // 
+            // chkBxShowPassword
+            // 
+            this.chkBxShowPassword.AutoSize = true;
+            this.chkBxShowPassword.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.chkBxShowPassword.Location = new System.Drawing.Point(29, 310);
+            this.chkBxShowPassword.Name = "chkBxShowPassword";
+            this.chkBxShowPassword.Size = new System.Drawing.Size(134, 23);
+            this.chkBxShowPassword.TabIndex = 4;
+            this.chkBxShowPassword.Text = "Show Password";
+            this.chkBxShowPassword.UseVisualStyleBackColor = true;
+            this.chkBxShowPassword.CheckedChanged += new System.EventHandler(this.chkBxShowPassword_CheckedChanged);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnRegister.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnRegister.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnRegister.Location = new System.Drawing.Point(64, 364);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(169, 36);
+            this.btnRegister.TabIndex = 5;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClear.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btnClear.Location = new System.Drawing.Point(64, 415);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(169, 36);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label5.Location = new System.Drawing.Point(60, 483);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(187, 19);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Already Have an Account?";
+            // 
+            // backToLogin
+            // 
+            this.backToLogin.AutoSize = true;
+            this.backToLogin.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.backToLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.backToLogin.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.backToLogin.Location = new System.Drawing.Point(95, 512);
+            this.backToLogin.Name = "backToLogin";
+            this.backToLogin.Size = new System.Drawing.Size(101, 19);
+            this.backToLogin.TabIndex = 0;
+            this.backToLogin.Text = "Back to Login";
+            this.backToLogin.Click += new System.EventHandler(this.backToLogin_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.AutoSize = true;
+            this.btnExit.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnExit.Location = new System.Drawing.Point(277, -1);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(31, 31);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "x";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(308, 592);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.chkBxShowPassword);
+            this.Controls.Add(this.txtBxConfirmPassword);
+            this.Controls.Add(this.txtBxPassword);
+            this.Controls.Add(this.txtBxUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.backToLogin);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Adobe Fan Heiti Std B", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Register";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registration";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBxUsername;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBxPassword;
+        private System.Windows.Forms.TextBox txtBxConfirmPassword;
+        private System.Windows.Forms.CheckBox chkBxShowPassword;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label backToLogin;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ RegistrationForm/Register.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistrationForm
+{
+    public partial class Register : Form
+    {
+      
+
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        OleDbCommand cmd = new OleDbCommand();
+        OleDbDataAdapter da = new OleDbDataAdapter();
+        OleDbConnection con = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=db_users.mdb");
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtBxUsername.Text = "";
+            txtBxPassword.Text = "";
+            txtBxConfirmPassword.Text = "";
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.ExitThread();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+
+            if (txtBxUsername.Text != string.Empty || txtBxPassword.Text != string.Empty && txtBxConfirmPassword.Text != string.Empty)
+            {
+                if(txtBxPassword.Text == txtBxConfirmPassword.Text)
+                {
+                    con.Open();
+                    string checkPassword = "SELECT * FROM tbl_users WHERE username='" + txtBxUsername.Text + "' AND password='" + txtBxPassword.Text + "'";
+                    cmd = new OleDbCommand(checkPassword, con);
+                    OleDbDataReader dr = cmd.ExecuteReader();
+
+                    if(dr.Read())
+                    {
+                        MessageBox.Show("Username is Already Exist, Please try again", "Try Again", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    } else
+                    {
+                        dr.Close();
+                        string register = "INSERT INTO tbl_users VALUES ('" + txtBxUsername.Text + "','" + txtBxPassword.Text + "')";
+                        cmd = new OleDbCommand(register, con);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Your Account is created, Please click the Login Below", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+
+                    con.Close();
+
+                } 
+                
+                else
+                {
+                    MessageBox.Show("Password Not Matched!", "Try Again", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            } 
+            
+            else
+            {
+                MessageBox.Show("Password or Username is Empty!", "Try Again!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+         
+           // MessageBox.Show("Created an Account!", "Success", MessageBoxButtons.OK);
+
+        }
+
+        private void backToLogin_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login login = new Login();
+            login.ShowDialog();
+        }
+
+        private void chkBxShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkBxShowPassword.Checked)
+            {
+                txtBxPassword.UseSystemPasswordChar = true;
+                txtBxConfirmPassword.UseSystemPasswordChar = true;
+            } else
+            {
+                txtBxPassword.UseSystemPasswordChar = false;
+                txtBxConfirmPassword.UseSystemPasswordChar = false;
+            }
+        }
+    }
+}
